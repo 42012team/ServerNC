4a0067d42029142af021c532cdb4c8a4539d01a1
Adaline <adelka1009@mail.ru>
2017-01-31 20:36:34 +0400

    auto-commit

--- src/classes/pessimisticLock/PessimisticLockingThread.java
+++ src/classes/pessimisticLock/PessimisticLockingThread.java
@@ -1,47 +1,33 @@
 package classes.pessimisticLock;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 public class PessimisticLockingThread extends Thread {
 
-    private static final int BLOCKING_TIME=20000;
-    private static List<Long> removingTime = new ArrayList<Long>();
-    private static List<Integer> prohibitedActionList = new ArrayList<Integer>();
+    private static final int BLOCKING_TIME = 20000;
+    private static Map<Long, Integer> prohibitedActionMap = new HashMap<Long, Integer>();
     private static Object monitor = new Object();
 
     public static boolean contains(int id) {
-        for (int i = 0; i < prohibitedActionList.size(); i++) {
-            if (prohibitedActionList.get(i) == id) {
-                return true;
-            }
-        }
-        return false;
+        return prohibitedActionMap.containsValue(id);
     }
 
     @Override
     public void run() {
         while (true) {
-            List<Integer> listForDeleting = new ArrayList<Integer>();
-            Date currentDate = new Date();
-            long time = currentDate.getTime();
-            for (int i = 0; i < prohibitedActionList.size(); i++) {
-                if (time >= removingTime.get(i)) {
-                    listForDeleting.add(i);
+            long currentTime = new Date().getTime();
+            long sleepingTime = currentTime;
+            Set<Long> timeToDeleteSet = prohibitedActionMap.keySet();
+            for (Long deletingTime : timeToDeleteSet) {
+                if (deletingTime <= currentTime) {
+                    prohibitedActionMap.remove(deletingTime);
                 } else {
-                    break;
+                    if (deletingTime - currentTime < sleepingTime) {
+                        sleepingTime = deletingTime - currentTime;
+                    }
                 }
             }
-            for (int i = 0; i < listForDeleting.size(); i++) {
-                prohibitedActionList.remove((int) listForDeleting.get(i));
-                removingTime.remove((int) listForDeleting.get(i));
-            }
-            long sleepingTime = 0;
-            if (!removingTime.isEmpty()) {
-                sleepingTime = removingTime.get(0) - currentDate.getTime();
-            }
-            if (sleepingTime > 0) {
+            if (sleepingTime <= BLOCKING_TIME) {
                 synchronized (monitor) {
                     try {
                         System.out.println("Поток блокировки уснул на " + sleepingTime / 1000 + " секунд");
@@ -60,38 +46,30 @@ public class PessimisticLockingThread extends Thread {
                     }
                 }
             }
-
         }
 
     }
 
-    public static void schedule(int id) {
-        Date currentDate = new Date();
-        Long time = currentDate.getTime();
-        prohibitedActionList.add(id);
-        removingTime.add(time + BLOCKING_TIME);
-        if (prohibitedActionList.size() == 1) {
-            synchronized (monitor) {
-                monitor.notify();
-            }
+    public static long schedule(int id) {
+        Long currentTime = new Date().getTime();
+        long timeToDelete=currentTime+BLOCKING_TIME;
+        prohibitedActionMap.put(timeToDelete, id);
+        synchronized (monitor) {
+            monitor.notify();
         }
+        return timeToDelete;
     }
 
     public static void unschedule(int id) {
-        if (prohibitedActionList.get(0) == id) {
-            prohibitedActionList.remove(prohibitedActionList.get(0));
-            removingTime.remove(removingTime.get(0));
-            synchronized (monitor) {
-                monitor.notify();
+        Set<Long> allKeys = prohibitedActionMap.keySet();
+        for (Long timeToDelete : allKeys) {
+            if (prohibitedActionMap.get(timeToDelete).equals(id)) {
+                prohibitedActionMap.remove(timeToDelete);
+                break;
             }
-        } else {
-            for (Integer ID : prohibitedActionList) {
-                if (ID.equals(id)) {
-                    removingTime.remove(removingTime.get(prohibitedActionList.indexOf(ID)));
-                    prohibitedActionList.remove(ID);
-                    break;
-                }
-            }
+        }
+        synchronized (monitor) {
+            monitor.notify();
         }
     }
 
